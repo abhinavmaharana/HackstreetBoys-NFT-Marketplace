a2b71858f60b9346023db60b575ac68f4e290305
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-02-11 01:44:47 +0530

    ✅ Added Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,10 @@ export default function Home() {
         <title>PsychoArt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className='max-w-8xl mx-auto px-8 sm:px-16'>
-        <section className='mt-4'>
-          <Header />
+        <section className='mt-6'>
+        <Header />
         </section>
       </main> 
     </div>
--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -1,19 +1,33 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 py-5 px-5 md:px-10">
+    <header className="top-0 z-50 grid grid-cols-2  py-2 px-5 md:px-10">
+        <div className="grid grid-cols-3">
         {/* Left */}
-        <div className="relative flex items-center h-10 cursor-pointer my-auto">
-          <h1>PsychoArt</h1>
+        <div className="relative flex items-center h-5 cursor-pointer my-auto space-x-6 text-center">
+          <h1 className="text-xl">PsychoArt</h1>
         </div>
-        {/* Middle */}
-        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        {/* Middle - Search */}
+        <div className="flex items-center md:border-2 bg-[#282828] border-[#282828] rounded-lg py-2 md:shadow-sm w-[300px] xl:ml-[-60px] 2xl:ml-[-80px] xl:w-[470px] 2xl:w-[400px] ml-[-30px]">
           <input
-            className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+            className="flex-grow pl-6 bg-[#282828]  outline-none text-sm text-white placeholder-white"
             type="text"
             placeholder={'Start your search'}
           />
         </div>
-        {/* Right */}
+      </div>
+      {/* Right */}
+      <div className="flex items-center justify-end space-x-12  text-white">
+        <h3 className="font-[Roboto] font-normal hover:text-[#A435F0] transition duration-150 ease-in">
+          Collections
+        </h3>
+        <h3 className="font-[Roboto] font-normal hover:text-[#A435F0] transition duration-150 ease-in">
+          Feature
+        </h3>
+        <h3 className="font-[Roboto] font-normal hover:text-[#A435F0] transition duration-150 ease-in">
+          FAQ
+        </h3>
+        <button className="bg-[#A435F0] py-3 px-6 rounded-md">Select Wallet</button>
+      </div>
     </header>
   )
 }
