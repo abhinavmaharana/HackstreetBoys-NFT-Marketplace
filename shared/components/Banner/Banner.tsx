ff4edff7167f8189c30d8c1321ff4fcc88273740
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-02-11 11:34:39 +0530

    :lipstick: UI fixes
    UI fixes

--- shared/components/Banner/Banner.tsx
+++ shared/components/Banner/Banner.tsx
@@ -1,33 +1,44 @@
+import Link from 'next/link'
+
 export default function Banner() {
   return (
-    <div className="max-w-8xl mx-auto px-8 sm:px-16 mt-8 xl:mt-[150px] 2xl:mt-[95px] xl:ml-[-18px] 2xl:ml-[-30px] items-center">
-        <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
-            <div className="grid grid-rows-3 gap-y-8">
-                <h1 className="text-7xl font-bold">Create, Sell & Collect Your Own Creative NFT</h1>
-                <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit.</p>
-                <div className="xl:mt-[-150px] space-x-4">
-                    <button className="text-white bg-gradient-to-b from-[#B75CFF] to-[#671AE4] px-16 py-4 shadow-md rounded-lg">Explore Now</button>
-                    <button className="bg-[#282828] outline-none px-12 py-4 shadow-md rounded-lg">Sell NFT</button>
-                </div>
-                <div className="xl:mt-[-250px] 2xl:mt-[-280px] grid grid-cols-3 xl:ml-[-80px]">
-                    <div className="text-center space-y-2">
-                        <h1 className="text-3xl">31+</h1>
-                        <p>Artworks</p>
-                    </div>
-                    <div className="text-center space-y-2">
-                        <h1 className="text-3xl">31+</h1>
-                        <p>Artworks</p>
-                    </div>
-                    <div className="text-center space-y-2">
-                        <h1 className="text-3xl">31+</h1>
-                        <p className="">Artworks</p>
-                    </div>
-                </div>
+    <div className="max-w-8xl mx-auto mt-8 items-center px-8 sm:px-16 xl:mt-[150px] xl:ml-[-18px] 2xl:mt-[95px] 2xl:ml-[-30px]">
+      <div className="grid grid-cols-1 items-center lg:grid-cols-2">
+        <div className="grid grid-rows-3 gap-y-8">
+          <h1 className="text-7xl font-bold">
+            Create, Sell & Collect Your Own Creative NFT
+          </h1>
+          <p className="text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            vulputate libero et velit.
+          </p>
+          <div className="space-x-4 xl:mt-[-150px]">
+            <Link href="#collection">
+              <button className="h-[60px] w-[200px] rounded-lg bg-gradient-to-b from-[#B75CFF] to-[#671AE4] text-white shadow-md hover:font-bold">
+                Explore Now
+              </button>
+            </Link>
+            <button className="h-[60px] w-[200px] rounded-lg bg-[#282828] shadow-md outline-none hover:font-bold">
+              Sell NFT
+            </button>
+          </div>
+          <div className="grid grid-cols-3 xl:mt-[-250px] xl:ml-[-80px] 2xl:mt-[-280px]">
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl">31+</h1>
+              <p>Artworks</p>
             </div>
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl">31+</h1>
+              <p>Artworks</p>
+            </div>
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl">31+</h1>
+              <p className="">Artworks</p>
+            </div>
+          </div>
         </div>
-        <div className="overflow-hidden">
-
-        </div>
+      </div>
+      <div className="overflow-hidden"></div>
     </div>
   )
 }
