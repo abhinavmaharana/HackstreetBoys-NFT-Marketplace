41260cb7bc3ad7d5ee90258a491bf01605b621ef
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-02-12 15:02:33 +0530

    ✅ Add Collection Page

---
+++ pages/nfts/[nftId].jsx
@@ -0,0 +1,95 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import NFTImage from '../../shared/components/nft/NFTImage'
+import GeneralDetails from '../../shared/components/nft/GeneralDetails'
+import ItemActivity from '../../shared/components/nft/ItemActivity'
+import Purchase from '../../shared/components/nft/Purchase'
+import Header from '../../shared/components/Header'
+
+
+const style = {
+    wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+    container: `container p-6`,
+    topContent: `flex`,
+    nftImgContainer: `flex-1 mr-4`,
+    detailsContainer: `flex-[2] ml-4`,
+}
+
+export default function Nft() {
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/jmPlM0DXhl17w4vgZyvlWpIeviWhUiSz'
+    )
+    return sdk.getNFTModule('0xEfa2Eb9FFc3c8DBf967373415D608f7fB41A129f')
+  }, [provider])
+
+  console.log(nftModule)
+
+  // get all NFTs in the collection
+  useEffect(() => {
+    if (!nftModule) return
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/jmPlM0DXhl17w4vgZyvlWpIeviWhUiSz'
+    )
+
+    return sdk.getMarketplaceModule(
+      '0xDFd05f728D29232e67Ec6f6e45750F42aa517cb6'
+    )
+
+  }, [provider])
+
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+  
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
