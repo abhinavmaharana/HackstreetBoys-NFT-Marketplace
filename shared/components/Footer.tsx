8027bc76ebfa7837de33cd568d93383049f35ec9
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-02-12 11:11:28 +0530

    🐛 Fixes Footer

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="grid grid-rows-1 md:grid-cols-5 px-24 py-14 gap-x-40">
+    <div className="grid grid-rows-1 md:grid-cols-4 px-24 py-14 gap-x-48">
       <div className="space-y-4 text-xs text-white">
         <h5 className="font-bold">ABOUT</h5>
         <p>How Airbnb works</p>
@@ -33,14 +33,14 @@ export default function Footer() {
         <p>Airbnb Plus</p>
         <p>Airbnb Luxe</p>
       </div>
-      <div className="space-y-4 text-xs text-white">
+      {/* <div className="space-y-4 text-xs text-white">
         <h5 className="font-bold">ABOUT</h5>
         <p>How Airbnb works</p>
         <p>Newsroom</p>
         <p>Investors</p>
         <p>Airbnb Plus</p>
         <p>Airbnb Luxe</p>
-      </div>
+      </div> */}
     </div>
   )
 }
