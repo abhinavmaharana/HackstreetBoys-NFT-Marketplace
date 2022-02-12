4cd336f04066ed4dabf2bded36040d95eae708da
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-02-12 15:17:41 +0530

    🐛 Fix in Footer

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -1,46 +1,30 @@
 export default function Footer() {
   return (
     <div className="grid grid-rows-1 md:grid-cols-4 px-24 py-14 gap-x-48">
-      <div className="space-y-4 text-xs text-white">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+      <div className="space-y-4 text-2xl text-white">
+        <h5 className="font-bold">Psycho Art</h5>
+        <p>Made the Website with love</p>
       </div>
       <div className="space-y-4 text-xs text-white">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <h5 className="font-bold">About</h5>
+        <p>Product</p>
+        <p>Resource</p>
+        <p>Term & Condition</p>
+        <p>FAQ</p>
       </div>
       <div className="space-y-4 text-xs text-white">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <h5 className="font-bold">Company</h5>
+        <p>Our Team</p>
+        <p>Partner With Us</p>
+        <p>Privacy & Policy</p>
+        <p>Features</p>
       </div>
       <div className="space-y-4 text-xs text-white">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
+        <h5 className="font-bold">Contact</h5>
+        <p>+91 887567645</p>
         <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>support@gmail.com</p>
       </div>
-      {/* <div className="space-y-4 text-xs text-white">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div> */}
     </div>
   )
 }
