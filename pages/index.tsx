a4d426bd53ec189e9842e00a322cbbeb8a86e65b
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-02-12 11:10:28 +0530

    :lipstick: Add Scroll Bar
    Add Scroll Bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,16 @@ import TopCreator from '../shared/components/topCreator'
 import Banner from '../shared/components/Banner/Banner'
 import Faq from '../shared/components/faq'
 import CollectNFT from '../shared/components/collectNFT'
-import SassyNinja from '../shared/components/threeModel'
 import Footer from '../shared/components/Footer'
 
 export default function Home() {
   return (
-    <div className=" ">
+    <div className="h-[100vh] scrollbar-thin scrollbar-track-gray-900 scrollbar-thumb-purple-400">
       <Head>
         <title>PsychoArt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="max-w-8xl mx-auto h-32 px-8 sm:px-16">
+      <main className="max-w-8xl mx-auto  px-8 sm:px-16 ">
         <section className="mt-4">
           <Header />
         </section>
